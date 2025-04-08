a6e86337622b55081aea2501f83b4aefb4dddfdf
YiminGao0113 <yg9bq@virginia.edu>
2025-04-08 16:12:56 -0400

    Need to update the ewight and gradient precision here for each run

--- Training_pytorch/train.py
+++ Training_pytorch/train.py
@@ -50,9 +50,9 @@ parser.add_argument('--c2cVari', default=0)
 current_time = datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
 
 args = parser.parse_args()
-args.wl_weight = 5            # weight precision
-args.wl_grad = 5              # gradient precision
-args.cellBit = 5              # cell precision (in V2.0, we only support one-cell-per-synapse, i.e. cellBit==wl_weight==wl_grad)
+args.wl_weight = 8            # weight precision
+args.wl_grad = 8              # gradient precision
+args.cellBit = 8              # cell precision (in V2.0, we only support one-cell-per-synapse, i.e. cellBit==wl_weight==wl_grad)
 args.max_level = 32           # Maximum number of conductance states during weight update (floor(log2(max_level))=cellBit) 
 args.c2cVari = 0.003          # cycle-to-cycle variation
 args.d2dVari = 0.0            # device-to-device variation
